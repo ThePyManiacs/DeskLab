027acd96ca4b1f8b2541b4e93b2f3f02269cfb21
Jonatas <joncortes.on@gmail.com>
2026-05-17 00:06:47 -0300

    FIX: addapt DragDrop to inherit from FlexBox

--- src/labweb/drag_drop.py
+++ src/labweb/drag_drop.py
@@ -1,12 +1,12 @@
-from src.labweb.containers.hover_emphasizing_flexbox import HoverEmphasizingFlexBox
+from src.labweb.containers.flexbox import FlexBox
 from src.labweb.system.mouse import Mouse
 from pygame.event import Event
 from typing import Any
 from src.labweb.constants import FlexDirection, HorizontalAlignment, VerticalAlignment
-from src.labweb.color import Color
+from src.labweb.properties.color import Color
 
 
-class DragDrop(HoverEmphasizingFlexBox):
+class DragDrop(FlexBox):
 
     def __init__(self,
                  width: int,
@@ -18,13 +18,12 @@ class DragDrop(HoverEmphasizingFlexBox):
                  vertical_alignment: str | VerticalAlignment = VerticalAlignment.CENTER,
                  corners_radius: tuple[int, int, int, int] | int = 0,
                  color: Color | tuple[int, int, int] | str = "BLACK",
-                 hover_emphasis_intensity: int = 20,
                  bounded: bool = True) -> None:
 
         self.__files: list[str] = []
         super().__init__(width, height, padding, space_between, flex_direction,
                          horizontal_alignment, vertical_alignment, corners_radius,
-                         color, hover_emphasis_intensity, bounded)
+                         color, bounded)
 
     def handle_event(self, *args: Any, **kwargs: Any) -> None:
         super().handle_event(*args, **kwargs)
@@ -56,8 +55,3 @@ class DragDrop(HoverEmphasizingFlexBox):
 
     def count_files(self) -> int:
         return len(self.__files)
-
-    def copy(self) -> "DragDrop":
-        instance = self._copy()
-        assert isinstance(instance, self.__class__)
-        return instance
