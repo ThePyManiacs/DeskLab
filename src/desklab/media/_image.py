79a19e6860a4537f1e7e2d1de8fbca639e28bc42
Jonatas <joncortes.on@gmail.com>
2026-05-29 13:57:47 -0300

    FEAT: remove unnecessary dependencies from Image and add suport for alpha channel

--- src/desklab/media/_image.py
+++ src/desklab/media/_image.py
@@ -1,59 +1,62 @@
 # fmt: off
-from typing import Any
-
-from PIL import Image as PilImage
-import numpy as np
-from desklab.entity_types import DisplayableEntity, ContainableEntity, CopiableEntity
 import os
+from typing import Any, Self, overload
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pygame import Surface
-from desklab._check import type_check, value_check, EndsWithValidationRule, EqualsValidationRule
+from desklab.entity_types import DisplayableEntity, ContainableEntity, CopiableEntity
+from desklab._check import type_check, value_check, RangeValidationRule
 # fmt: on
-
-_valid_image_formats = (".png", ".jpg", ".jpeg", ".bmp", ".gif")
 
 
 @type_check
 class Image(DisplayableEntity, ContainableEntity, CopiableEntity):
 
-    def __init__(self, image: str | np.ndarray) -> None:
-        self.__set_image_surface(image)
-        super().__init__(width=self.get_matrix().shape[0],
-                         height=self.get_matrix().shape[0])
+    @overload
+    def __init__(self, image_source: str) -> None: ...
 
-    def get_matrix(self) -> np.ndarray: return self.__image_matrix
+    @overload
+    def __init__(self, image_source: Surface) -> None: ...
 
-    @value_check(image_path=EndsWithValidationRule(_valid_image_formats, f"Image format must be one of {_valid_image_formats}"))
-    def __extract_image_data_from_path(self, image_path: str) -> np.ndarray:
-        img = PilImage.open(image_path).convert("RGB")
-        return np.array(img)
+    def __init__(self, image_source: str | Surface) -> None:
+        self.__load_surface(image_source)
 
-    @value_check(image_ndim=EqualsValidationRule(3, "Image must have 3 dimensions"),
-                 image_color_channels=EqualsValidationRule(3, "Image must have 3 color channels"))
-    def __validate_dimensions(self, image_ndim: int, image_color_channels: int) -> None:
-        pass
+        super().__init__(
+            width=self.__image_surface.get_width(),
+            height=self.__image_surface.get_height()
+        )
 
-    def __set_image_surface(self, image: str | np.ndarray) -> None:
+    def __load_surface(self, source: str | Surface) -> None:
+        if isinstance(source, Surface):
+            self.__image_surface = source.copy()
+        else:
+            self.__image_surface = pygame.image.load(source)
 
-        if isinstance(image, str):
-            image = self.__extract_image_data_from_path(image)
+        if self.__image_surface.get_alpha() is not None or self.__image_surface.get_masks()[3] != 0:
+            self.__image_surface = self.__image_surface.convert_alpha()
+        else:
+            self.__image_surface = self.__image_surface.convert()
 
-        self.__validate_dimensions(image.ndim, image.shape[2])
-        image = np.transpose(image, (1, 0, 2))
-        image = np.ascontiguousarray(image)
+    def __get_surface(self) -> Surface:
+        return self.__image_surface.copy()
 
-        self.__image_matrix = image
-        self.__image_surface = pygame.surfarray.make_surface(image)
+    @value_check(percentage=RangeValidationRule(0, variable_name="percentage"))
+    def rescaled(self, percentage: float) -> Self:
+        new_width = int(self.get_width() * percentage)
+        new_height = int(self.get_height() * percentage)
+        new_surface = pygame.transform.scale(self.__get_surface(),
+                                             (new_width, new_height))
+        return self.copy(image_source=new_surface)
 
-    def rescale(self, width: int, height: int) -> None:
-        self.__image_surface = pygame.transform.scale(
-            self.__image_surface, (width, height))
-        self._set_width(width)
-        self._set_height(height)
+    @value_check(width=RangeValidationRule(0, variable_name="width"),
+                 height=RangeValidationRule(0, variable_name="height"))
+    def resized(self, width: int, height: int) -> Self:
+        new_surface = pygame.transform.scale(self.__get_surface(),
+                                             (width, height))
+        return self.copy(image_source=new_surface)
 
     def display(self, screen: Surface) -> None:
         screen.blit(self.__image_surface, (self.get_x(), self.get_y()))
 
     def _get_copy_replacement_map(self) -> dict[str, Any]:
-        return {"image": self.get_matrix().copy()}
+        return {"image_source": self.__get_surface()}
